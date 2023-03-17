8fd271478518775dbf77cf05c816adaaeb5d4e8c
Svaniel <svanberg.d@gmail.com>
2023-03-17 14:49:41 +0100

    Added settings

--- app/app.py
+++ app/app.py
@@ -15,6 +15,7 @@ db = mysql.connector.connect(
 # create user table
 cursor = db.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
+cursor.execute("INSERT INTO users (name, email, password) VALUES ('John Smith', 'johnsmith@example.com', 'mypassword'), ('Jane Doe', 'janedoe@example.com', 'mypassword'), ('Robert Johnson', 'robertjohnson@example.com', 'mypassword'),('Alice Brown', 'alicebrown@example.com', 'mypassword'), ('David Lee', 'davidlee@example.com', 'mypassword'), ('Emily Davis', 'emilydavis@example.com', 'mypassword'), ('William Chen', 'williamchen@example.com', 'mypassword'), ('Olivia Nguyen', 'olivianguyen@example.com', 'mypassword'), ('Michael Kim', 'michaelkim@example.com', 'mypassword'), ('Sophia Patel', 'sophiapatel@example.com', 'mypassword')")
 
 
 @app.route('/')
@@ -60,12 +61,12 @@ def register():
 def login():
     if request.method == 'POST':
         # Get the form data
-        name = request.form['username']
+        name = request.form['email']
         password = request.form['password']
 
         # Query the database for the user with the matching email and password
         cursor = db.cursor()
-        cursor.execute('SELECT * FROM users WHERE name = %s AND password = %s', (name, password))
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (name, password))
         user = cursor.fetchone()
         cursor.close()
 
@@ -89,32 +90,40 @@ def profile():
 
     # Retrieve user data from database
     user_id = session['user_id']
-    cursor = mysql.connection.cursor()
+    cursor = db.cursor()
     cursor.execute('SELECT * FROM users WHERE id = %s', (user_id,))
     user = cursor.fetchone()
-    cursor.close()
-
-    # Render profile template with user data
-    return render_template('profile.html', user=user)
+    # cursor.close()
+    if user:
+        # Render profile template with user data
+        return render_template('profile.html', user=user)
+    else:
+        return "User not found"
 
 @app.route('/settings/<int:id>', methods=['GET', 'POST'])
 def settings(id):
+        # Check if user is logged in
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
     if request.method == 'POST':
-        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+
         cursor = db.cursor()
-        cursor.execute("INSERT INTO users (email) VALUES (%s) WHERE id = %s", (email, id))
+        cursor.execute("UPDATE users SET name = %s, password = %s WHERE id = %s", (name, password, id))
         db.commit()
-        return redirect(url_for('profile'))
-    
-    cursor = db.cursor()
-    cursor.execute("SELECT * from users WHERE id = %s", (id,))
-    user = cursor.fetchone()
-    cursor.close()
-    
-    if user:
-        return render_template('settings.html', user=user)
+        return redirect(url_for('settings', id=id))
     else:
-        return "User not found"
+        cursor = db.cursor()
+        cursor.execute("SELECT * from users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+        cursor.close()
+        
+        if user:
+            return render_template('settings.html', user=user)
+        else:
+            return "User not found"
 
 
 @app.route('/logout')
@@ -124,4 +133,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)#, host='0.0.0.0')
